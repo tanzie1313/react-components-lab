f7870e0042732af22881b407aaecf561d5a43783
tanzie1313 <tanziehickory@gmail.com>
2025-03-10 22:34:14 -0700

    final

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,15 +1,18 @@
 
-import react from 'react';
-import './WeatherForecast.jsx';
+import React from 'react';
+import "./WeatherForecast.css"; 
 import WeatherIcon from "../WeatherIcon/WeatherIcon.jsx"
 import WeatherData from "../WeatherData/WeatherData.jsx"
 
-const WeatherForecast = ({day, icon, alt, conditions, time}) => {
-    return (
-     <div className="weather">
-        <WeatherIcon icon={icon} alt={alt} />
-          <WeatherData day={day} conditions={conditions} time={time} /> 
-     </div>
-    );
-    }
-    export default WeatherForecast;
+const WeatherForecast = ({day,icon,alt, conditions, time}) => {
+
+return (
+
+<div className="weather">
+<WeatherIcon icon={icon} alt={alt}/>
+  <WeatherData day={day} conditions={conditions} time={time}/>
+ 
+</div>
+);
+};
+export default WeatherForecast;
